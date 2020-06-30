19933f7b3670107ac71707156793263c3e1ccaea
jeff <bbmyth@gmail.com>
2020-06-30 09:37:58 +0800

    使用post_bsm_xxx系列Signal触发

--- trigger_core/signals.py
+++ trigger_core/signals.py
@@ -12,6 +12,35 @@ from api_core.signals import bsm_after_update
 from api_core.signals import bsm_before_delete
 from api_core.signals import bsm_after_delete
 
+from api_basebone.signals import post_bsm_create, post_bsm_delete
+
+
+@receiver(post_bsm_create, dispatch_uid='post_bsm_create_by_trigger')
+def handler_post_bsm_create(sender, instance, create, request, old_instance, **kwargs):
+    """数据保存后
+    """
+    print('数据保存后啦！')
+    trigger_services.handle_triggers(
+        request,
+        const.TRIGGER_EVENT_AFTER_CREATE if create else const.TRIGGER_EVENT_AFTER_UPDATE,
+        id=instance.pk,
+        old_inst=old_instance,
+        new_inst=instance,
+        app=sender._meta.app_label,
+        model=sender._meta.model_name,
+    )
+
+@receiver(post_bsm_delete, dispatch_uid='post_bsm_delete_by_trigger')
+def handler_post_bsm_delete(sender, instance, request, **kwargs):
+    trigger_services.handle_triggers(
+        request,
+        const.TRIGGER_EVENT_AFTER_DELETE,
+        id=instance.pk,
+        old_inst=instance,
+        new_inst=None,
+        app=sender._meta.app_label,
+        model=sender._meta.model_name,
+    )
 
 @receiver(bsm_before_create, sender=ApiViewSet, dispatch_uid='before_create')
 def handle_before_create(sender, apiViewSet, new_inst, **kwargs):
