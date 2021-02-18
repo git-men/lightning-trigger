0cce8b238a96f3687a62a8ad4bd4e4a386bd6db8
jeff <bbmyth@gmail.com>
2021-02-18 20:06:56 +0800

    支持创建和删除前的触发事件

--- trigger_core/signals.py
+++ trigger_core/signals.py
@@ -5,7 +5,7 @@ from api_core.api.views import ApiViewSet
 from . import const
 from .services import trigger_services
 
-from api_basebone.signals import post_bsm_create, post_bsm_delete
+from api_basebone.signals import post_bsm_create, post_bsm_delete, before_bsm_create, before_bsm_delete
 
 
 @receiver(post_bsm_create, dispatch_uid='post_bsm_create_by_trigger')
@@ -36,79 +36,26 @@ def handler_post_bsm_delete(sender, instance, request, **kwargs):
     )
 
 
-def handle_before_create(sender, apiViewSet, new_inst, **kwargs):
-    api = apiViewSet.api
+@receiver(before_bsm_create, dispatch_uid='before_bsm_create_by_trigger')
+def handle_before_create(sender, instance, create, request, **kwargs):
     trigger_services.handle_triggers(
-        apiViewSet.request,
-        const.TRIGGER_EVENT_BEFORE_CREATE,
-        id=new_inst.id,
+        request,
+        const.TRIGGER_EVENT_BEFORE_CREATE if create else const.TRIGGER_EVENT_BEFORE_UPDATE,
+        id=None if create else instance['id'],
         old_inst=None,
-        new_inst=new_inst,
-        app=api.app,
-        model=api.model,
+        new_inst=instance,
+        app=sender._meta.app_label,
+        model=sender._meta.model_name
     )
 
-
-def handle_after_create(sender, apiViewSet, new_inst, **kwargs):
-    api = apiViewSet.api
+@receiver(before_bsm_delete, dispatch_uid='before_bsm_delete_by_trigger')
+def handle_before_delete(sender, instance, request, **kwargs):
     trigger_services.handle_triggers(
-        apiViewSet.request,
-        const.TRIGGER_EVENT_AFTER_CREATE,
-        id=new_inst.id,
-        old_inst=None,
-        new_inst=new_inst,
-        app=api.app,
-        model=api.model,
-    )
-
-
-def handle_before_update(sender, apiViewSet, old_inst, new_inst, **kwargs):
-    api = apiViewSet.api
-    trigger_services.handle_triggers(
-        apiViewSet.request,
-        const.TRIGGER_EVENT_BEFORE_UPDATE,
-        id=new_inst.id,
-        old_inst=old_inst,
-        new_inst=new_inst,
-        app=api.app,
-        model=api.model,
-    )
-
-
-def handle_after_update(sender, apiViewSet, old_inst, new_inst, **kwargs):
-    api = apiViewSet.api
-    trigger_services.handle_triggers(
-        apiViewSet.request,
-        const.TRIGGER_EVENT_AFTER_UPDATE,
-        id=new_inst.id,
-        old_inst=old_inst,
-        new_inst=new_inst,
-        app=api.app,
-        model=api.model,
-    )
-
-
-def handle_before_delete(sender, apiViewSet, old_inst, **kwargs):
-    api = apiViewSet.api
-    trigger_services.handle_triggers(
-        apiViewSet.request,
+        request,
         const.TRIGGER_EVENT_BEFORE_DELETE,
-        id=old_inst.id,
-        old_inst=old_inst,
+        id=instance.pk,
+        old_inst=instance,
         new_inst=None,
-        app=api.app,
-        model=api.model,
-    )
-
-
-def handle_after_delete(sender, apiViewSet, old_inst, **kwargs):
-    api = apiViewSet.api
-    trigger_services.handle_triggers(
-        apiViewSet.request,
-        const.TRIGGER_EVENT_AFTER_DELETE,
-        id=old_inst.id,
-        old_inst=old_inst,
-        new_inst=None,
-        app=api.app,
-        model=api.model,
+        app=sender._meta.app_label,
+        model=sender._meta.model_name
     )
